81a9daf0dd8740226048acb314bc992c4816ae96
aninocoder <kaleidescoffee@gmail.com>
2018-04-16 07:52:10 +0800

    Scene integration and object interaction
    Integrated scene and object art, as well as played around with the drag and drop mechanics for rings 1 and 2

---
+++ Assets/Scripts/DragDropSpring.cs
@@ -0,0 +1,36 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Modified version of https://answers.unity.com/questions/618546/how-to-drag-and-throw-2d-objects.html  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using UnityEngine;
+using System.Collections;
+ 
+ public class DragDropSpring : MonoBehaviour 
+ {
+     [SerializeField]
+     private SpringJoint2D spring;
+ 
+     void Awake()
+     {
+    	spring.connectedAnchor = transform.position;//i want the anchor position to start at the object's position
+		spring.enabled = false;
+     }
+ 
+     void OnMouseDown()
+     {
+     	spring.enabled = true;//I'm reactivating the SpringJoint2D component each time I'm clicking on my object becouse I'm disabling it after I'm releasing the mouse click so it will fly in the direction i was moving my mouse
+     }
+ 
+     void OnMouseDrag()        
+     {
+		if (spring.enabled) 
+		{
+			Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);//getting cursor position
+			spring.connectedAnchor = cursorPosition;//the anchor get's cursor's position
+		}
+     }
+     
+     void OnMouseUp()        
+     {
+     	spring.enabled = false;//disabling the spring component
+     }
+ }
