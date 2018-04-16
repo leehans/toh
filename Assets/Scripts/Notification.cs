ce038171c6b99f91bd01f7a3c65a47d3c2c23625
aninocoder <kaleidescoffee@gmail.com>
2018-04-16 20:10:03 +0800

    Implement filter rule and further fine-tuned physics
    Also added an EventBroadcaster system

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notification : MonoBehaviour 
+{
+	[SerializeField]
+	private Text text;
+
+	void OnDestroy()
+	{
+		StopAllCoroutines();
+	}
+
+	public void Display(string message)
+	{
+		if (text.text.Equals(message) && text.gameObject.activeInHierarchy) return;
+		text.text = message;
+		StartCoroutine("DelayHide", 3.0f);
+	}
+
+	#region  Coroutines
+	private IEnumerator DelayHide(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		text.gameObject.SetActive(false);
+		text.text = string.Empty;
+	}
+	#endregion // Coroutines
+}
